cfb69064f0644193d11661c50b4d28cad352656b
Abner <1228428816@qq.com>
2025-05-31 18:19:49 +0800

    chore: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,21 +5,26 @@ import sitemap from '@astrojs/sitemap';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-	markdown: {
-		shikiConfig: {
-			theme: 'dracula',
-		},
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+
+  markdown: {
+      shikiConfig: {
+          theme: 'dracula',
+      },
 	},
 
-	image: {
-		domains: ['unsplash.com', 'photos.luckyabner.top'],
+  image: {
+      domains: ['unsplash.com', 'photos.luckyabner.top'],
 	},
 
-	vite: {
-		plugins: [tailwindcss()],
+  vite: {
+      plugins: [tailwindcss()],
 	},
+
+  adapter: vercel(),
 });
