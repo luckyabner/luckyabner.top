64910ccb6288016af11e7e0e9b56679e55bd9424
luckyabner <1228428816@qq.com>
2026-07-26 19:15:43 +0800

    better ui

--- src/content/projects.ts
+++ src/content/projects.ts
@@ -2,35 +2,34 @@ interface Project {
   title: string;
   description: string;
   url: string;
-  icon: string;
+  icon?: string;
 }
 
 export const projects: Project[] = [
   {
-    title: "Shutter",
+    title: "Hexagram",
     description:
-      "A photo display website based on Nextjs and Tencent Cloud COS.",
-    url: "https://shutter.luckyabner.top/",
-    icon: "./icons/shutter.ico",
+      "AI Divination PWA app",
+    url: "https://yao.luckyabner.top/",
   },
   {
     title: "MusicRoast",
     description:
       "An AI-powered application that provides different reviews of users' playlists.",
     url: "https://music.luckyabner.top/",
-    icon: "./icons/musicroast.ico",
+    icon: "/icons/musicroast.ico",
   },
   {
     title: "HexVerse",
     description: "An AI-powered fortune-telling application.",
     url: "https://hexverse.luckyabner.top/",
-    icon: "./icons/hexverse.ico",
+    icon: "/icons/hexverse.ico",
   },
   {
     title: "EchoMemo",
     description:
       "An AI-enhanced thought capturing platform that provides intelligent feedback on your ideas.",
     url: "https://echo.luckyabner.top/",
-    icon: "./icons/echomemo.ico",
+    icon: "/icons/echomemo.ico",
   },
 ];
